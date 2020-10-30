d3a5c3b0100e89715d2a88b4c295d906b58d8b66
Eleuterio Fulaho Notico <eleuterio3d@gmail.com>
2020-10-30 09:30:00 +0200

    first commit

---
+++ database/migrations/2020_10_30_071729_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id');
+            $table->string('descricao');
+            $table->decimal('preco', 10, 2);
+            $table->integer('quantidade');
+            $table->
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
